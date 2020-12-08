288ed70f89f6b375940b130d89f12449de39d7a3
wang.haoyu <228233464@qq.com>
2020-12-08 20:55:59 +0800

    feat: request.body获取postData
    基于Promise封装异步获得post请求的request data

--- app.js
+++ app.js
@@ -1,48 +1,76 @@
-const handlerBolgRouter = require("./src/router/blog.js")
-const handleUserRouter = require("./src/router/user.js")
-const queryString = require('querystring')
-
+const handlerBolgRouter = require("./src/router/blog.js");
+const handleUserRouter = require("./src/router/user.js");
+const queryString = require("querystring");
+const { resolve } = require("path");
 
 /* 
   app.js只处理入口(公共参数和公共逻辑) 各个路由单独使用func处理(关注各自数据)
   各个路由页面关心路由返回的数据 并不关心res.end等等之类的 
   只返回数据 而app.js处理 请求和返回
   总结：app.js处理请求和响应，各个router处理各自路由的返回数据。
-*/ 
+*/
 
-const serverHanlder = (req,res) => {
+const getPostData = (req) => {
+  return new Promise((res, rej) => {
+    if (req.method !== "post") {
+      resolve({});
+      return;
+    }
+    if (req.header["Content-type"] !== "application/json") {
+      resolve({});
+      return;
+    }
+    let postData = "";
+    req.on("data", (chunk) => {
+      postData += chunk.toString();
+    });
+    req.on("end", () => {
+      if (!postData) {
+        resolve({});
+        return;
+      } else {
+        resolve(JSON.parse(postData));
+      }
+    });
+  });
+};
+
+const serverHanlder = (req, res) => {
   // 返回格式
-  res.setHeader('Content-Type','application/json;charset=utf-8;')
+  res.setHeader("Content-Type", "application/json;charset=utf-8;");
 
-
-  const url = req.url
+  const url = req.url;
   // req添加path属性 每个router文件直接使用
-  req.path = url.split('?')[0]
+  req.path = url.split("?")[0];
 
   // 解析query
-  req.query = queryString.parse(url.split('?')[1]) 
+  req.query = queryString.parse(url.split("?")[1]);
 
-  // 处理blog路由
-  const blogData = handlerBolgRouter(req,res)
+  getPostData(req).then((postData) => {
+    // 获得postData
+    req.body = postData;
 
-  if(blogData) {
-    res.end(JSON.stringify(blogData))
-    return
-  }
+    // 处理blog路由
+    const blogData = handlerBolgRouter(req, res);
 
-  // 处理userData
-  const userData = handleUserRouter(req,res)
+    if (blogData) {
+      res.end(JSON.stringify(blogData));
+      return;
+    }
 
-  if(userData) {
-    res.end(JSON.stringify(userData))
-    return
-  }
+    // 处理userData
+    const userData = handleUserRouter(req, res);
 
-  // 未命中路由 返回404状态码 第二个参数为status描述 不传可重载
-  res.writeHead(404,{'Content-Type':'text/plain'}) // 改变状态码
-  res.write('404 Not find') // 返回文本内容
-  res.end() // 结束
+    if (userData) {
+      res.end(JSON.stringify(userData));
+      return;
+    }
 
-}
+    // 未命中路由 返回404状态码 第二个参数为status描述 不传可重载
+    res.writeHead(404, { "Content-Type": "text/plain" }); // 改变状态码
+    res.write("404 Not find"); // 返回文本内容
+    res.end(); // 结束
+  });
+};
 
-module.exports = serverHanlder
+module.exports = serverHanlder;
